898de66dfa20c36503dcc2cb23b69710b846f291
Apprentice <apprentice@dbc27.local>
2013-10-23 23:37:11 -0700

    ar-student-schema

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,33 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+
+  validates :email, format: { with: /.+@.+\..+.+/, message: "invalid email"}
+  validates :email, uniqueness: true
+  validate :age_must_be_greater_than_five
+  validates :phone, length: {minimum: 10}
+
+  def age_must_be_greater_than_five
+    if age < 5
+      errors.add(:birthday, "can't be less than five year's old")
+    end
+  end
+
+  def name 
+    self.first_name + " " + self.last_name
+  end
+
+  def age
+    now = Date.today
+    age = now.year - self.birthday.year 
+  end
+
+
 end
+
+
+# sam = Student.create(first_name: "sam", last_name: "something")
+# sam.first_name
+# sam.last_name
+# sam.name
+
